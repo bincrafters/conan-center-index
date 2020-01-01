b69789b07bbb9e7ce22bf93faab192e6c6820060
Croydon <git@cr0ydon.com>
2020-01-01 20:20:04 +0100

    Add outdated openssl

--- recipes/cctz/all/conanfile.py
+++ recipes/cctz/all/conanfile.py
@@ -27,6 +27,10 @@ class CCTZConan(ConanFile):
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
 
+    required = (
+        "OpenSSL/1.0.2r@conan/stable"
+    )
+
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
